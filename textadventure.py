1cf30fe96e0ff88d2004ad33a0cd1a6806539d72
Simon Tobias Lund <simon.tobias.lund@gmaikl.com>
2014-03-31 14:24:54 +0200

    hohohoh

--- textadventure.py
+++ textadventure.py
@@ -4,7 +4,7 @@ Created on Mon Mar 31 12:59:55 2014
 
 @author: Simon
 """
-
+import time
 alleklasser = ["warrior", "priest", "archer", "jew"]
 
 klasse = raw_input("choose your class:")
@@ -19,24 +19,48 @@ if valgtklasse == "":
     print klasse + " is not a valid class"
 else:
     print "you are a(n) " + valgtklasse
-if valgtklasse == warrior:
+if valgtklasse == "warrior":
     hitpoint = 20
     damage = 5
     specialaction = 0
-    print "you have" + hitpoint + "hitpoints"
-    print "you deal " + damage + "damage"
+    print "you have " + str(hitpoint) + " hitpoints"
+    print "you deal " + str(damage) + " damage"
     print "your special action is none"
-elif valgtklasse == priest:
+elif valgtklasse == "priest":
     hitpoint = 15
     damage = 7
     specialaction = 1
-    print "you have" + hitpoint + "hitpoints"
-    print "you deal " + damage + "damage"
+    print "you have " + str(hitpoint) + " hitpoints"
+    print "you deal " + str(damage) + " damage"
     print "your special action is heal"
-elif valgtklasse == archer:
+elif valgtklasse == "archer":
     hitpoint = 15
     damage = 4
     specialaction = 2
-    print "you have" + hitpoint + "hitpoints"
-    print "you deal " + damage + "damage"
+    print "you have " + str(hitpoint) + " hitpoints"
+    print "you deal " + str(damage) + " damage"
     print "your special action is snipe"
+elif valgtklasse == "jew":
+    hitpoint = 20
+    damage = 20
+    specialaction = 3
+    print "you have " + str(hitpoint) + " hitpoints"
+    print "you deal " + str(damage) + " damage"
+    print "your special action is jew jitsu"
+time.sleep(15)
+print "you are in a dark room, in the room there is a door,"
+print "a window, a book shelf, and a lamp"
+alleactions = ["use lamp", "take book", "open window", "open door"]
+
+action = raw_input("choose your action:")
+valgtaction = ""
+
+for kl in alleactions:
+    if klasse == kl:
+        valgtaction = klasse
+        break
+    
+if valgtaction == "":
+    print action + " is not a valid action"
+else:
+    print "yout try to" + valgtaction
