1678582798c11fc9fe56f1fc637a532b91a564b6
Simon Tobias Lund <simon.tobias.lund@gmaikl.com>
2014-04-02 12:52:42 +0200

    bababebe

--- textadventure.py
+++ textadventure.py
@@ -5,52 +5,66 @@ Created on Mon Mar 31 12:59:55 2014
 @author: Simon
 """
 import time
-def klassen(a, b, c):
-        hitpoint = a
-        damage = b
-        specialaction = c
-        print "you have " + str(hitpoint) + " hitpoints"
-        print "you deal " + str(damage) + " damage"
-alleklasser = ["warrior", "priest", "archer", "jew"]
-
-klasse = raw_input("choose your class:")
-valgtklasse = ""
-
-for kl in alleklasser:
-    if klasse == kl:
-        valgtklasse = klasse
-        break
-    
-if valgtklasse == "":
-    print klasse + " is not a valid class"
-else:
-    print "you are a(n) " + valgtklasse
-if valgtklasse == "warrior":
-    klassen(20, 5, 0)
-    print "your special action is none"
-elif valgtklasse == "priest":
-    klassen(15, 7, 1)
-    print "your special action is heal"
-elif valgtklasse == "archer":
-    klassen(17, 4, 2)
-    print "your special action is snipe"
-elif valgtklasse == "jew":
-    klassen(20, 20, 3)
-    print "your special action is jew jitsu"
-time.sleep(15)
 print "you are in a dark room, in the room there is a door,"
 print "a window, a book shelf, and a lamp"
 alleactions = ["use lamp", "take book", "open window", "open door"]
 
-action = raw_input("choose your action:")
+
+
+class CharacterSelect:
+    valgtklasse = ""
+    alleklasser = ["warrior", "priest", "archer", "jew"]
+    def velgKlasse(self, duSkrev):
+        for kl in self.alleklasser:
+            if duSkrev == kl:
+                self.valgtklasse = duSkrev
+                break
+         
+    def klassen(a, b, c, d):
+        hitpoint = a
+        damage = b
+        specialaction = c
+        specialactionnavn = d
+        print "you have " + str(hitpoint) + " hitpoints"
+        print "you deal " + str(damage) + " damage"
+        print "your special action is " + str(specialactionnavn)
+        name = raw_input("choose your name:")
+        print "your name is " + name
+    def klasser(class1, class2, class3, class4):
+        if valgtklasse == "":
+            print klasse + " is not a valid class"
+        else:
+            print "you are a(n) " + valgtklasse
+        if valgtklasse == str(class1):
+            klassen(20, 5, 0, "none")
+        
+        elif valgtklasse == str(class2):
+            klassen(15, 7, 1, "heal")
+        
+        elif valgtklasse == str(class3):
+            klassen(17, 4, 2, "snipe")
+        
+        elif valgtklasse == str(class4):
+            klassen(20, 20, 3, "jew-jitsu")
+
+
+cs = CharacterSelect()
+skrev = raw_input("choose your class: ")
+hero = cs.velgKlasse(skrev)
+
+skrevetAction = raw_input("choose your action: ")
 valgtaction = ""
 
-for kl in alleactions:
-    if klasse == kl:
-        valgtaction = klasse
+for action in alleactions:
+    if action == skrevetAction:
+        valgtaction = skrevetAction
         break
     
 if valgtaction == "":
     print action + " is not a valid action"
 else:
     print "you try to" + valgtaction
+cs.hitpoint -= 5
+
+
+exit()
